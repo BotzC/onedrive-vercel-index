49a4a0040cb32cf5d44dc593bd218a7003957231
Botz <127378435+BotzC@users.noreply.github.com>
2024-01-18 14:04:46 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -55,6 +55,10 @@ module.exports = {
       name: 'GitHub',
       link: 'https://github.com/',
     },
+     {
+      name: 'Telegram',
+      link: 'https://t.me/Botz_C',
+    },
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
